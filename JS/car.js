f9eda67c246753f546f1af04f2cbe51537664e34
nikolayhotim1 <hotim_nikolay@mail.ru>
2021-06-22 20:04:13 +0300

    first commit

---
+++ JS/car.js
@@ -0,0 +1,15 @@
+let car1 = {
+    name: "Reno Stepway",
+    isTurnOn: false,
+    speed: 0,
+    engine: {
+        v: 1.6,
+        horsePower: 120,
+        isTurnOn: false
+    },
+    start: function() {
+        this.isTurnOn = true;
+        this.speed = 10;
+        this.engine.isTurnOn = true;
+    }  
+};
